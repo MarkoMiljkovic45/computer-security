9e187c30f8340da2bde26bf87f0be373068129d4
MarkoMiljkovic45 <miljkovicmarko45@gmail.com>
2023-03-19 21:43:26 +0100

    Add shell app

---
+++ lab-1/src/main/java/hr/fer/srs/PasswordManager.java
@@ -0,0 +1,20 @@
+package hr.fer.srs;
+
+/**
+ * Password Manager Application
+ * <p>
+ * It works like a console that supports 4 commands:
+ * <ul>
+ *     <li>init [master-password]                         - Initializes the master password</li>
+ *     <li>get [master-password] [web-address]            - Returns the password for a given web address</li>
+ *     <li>put [master-password] [web-address] [password] - Sets a password for a given web address</li>
+ *     <li>quit                                           - Closes the application</li>
+ * </ul>
+ */
+public class PasswordManager
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
