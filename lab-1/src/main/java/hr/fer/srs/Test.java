828dc3ab099ab9d698f5e97b2eaa137ff9c2701c
MarkoMiljkovic45 <miljkovicmarko45@gmail.com>
2023-03-26 17:08:08 +0200

    Test SecureRandom

--- lab-1/src/main/java/hr/fer/srs/Test.java
+++ lab-1/src/main/java/hr/fer/srs/Test.java
@@ -3,12 +3,15 @@ package hr.fer.srs;
 import hr.fer.srs.util.Util;
 
 import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class Test {
 
-    public static void main(String[] args) {
-        SecureRandom random = new SecureRandom("master".getBytes(StandardCharsets.UTF_8));
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        random.setSeed("master".getBytes(StandardCharsets.UTF_8));
         byte[] arr = new byte[16];
         random.nextBytes(arr);
         System.out.println(Util.byteToHex(arr));
